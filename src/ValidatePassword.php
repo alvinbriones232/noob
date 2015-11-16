83f571977325153fcd3fa07b5a4b40a1dc27a4c2
alvinbriones232 <alvinbriones232@gmail.com>
2015-11-16 16:23:19 +0800

    changes

--- src/ValidatePassword.php
+++ src/ValidatePassword.php
@@ -1,9 +1,9 @@
 <?php
 class ValidatePassword {
-	const MIN_LENGTH = 1;
-	const MAX_LENGTH = 4;
+	/*const MIN_LENGTH = 1;
+	const MAX_LENGTH = 4;*/
 	public function validLength($password) {
 		$passLength = strlen($password);
-		return $passLength >= self::MIN_LENGTH && $passLength <=  self::MAX_LENGTH;
+		return $passLength >= 1 && $passLength <= 4;
 	}
 }
