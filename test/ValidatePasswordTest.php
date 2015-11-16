4563d9f535d2f55c2d1f1eb680b40eddbf81c117
alvinbriones232 <alvinbriones232@gmail.com>
2015-11-16 16:19:19 +0800

    changes

--- test/ValidatePasswordTest.php
+++ test/ValidatePasswordTest.php
@@ -2,6 +2,6 @@
 class ValidatePasswordTest extends PHPUnit_Framework_TestCase {
 	public function testValidLength() {
 		$valpass = new ValidatePassword();
-		$this->assertFalse($valpass->validLength('1234456456456'));
+		$this->assertFalse($valpass->validLength('123456789'));
 	}
 }
