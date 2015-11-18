d6bf7e5de6a9e90e0cb6c9f5e65551e136d25edb
alvinbriones232 <alvinbriones232@gmail.com>
2015-11-18 15:15:46 +0800

    test

--- OutputTest.php
+++ OutputTest.php
@@ -1,16 +1,17 @@
 <?php
-class OutputTest extends PHPUnit_Framework_TestCase
-{
-    public function testExpectFooActualFoo()
+    class OutputTest extends PHPUnit_Framework_TestCase
     {
-        $this->expectOutputString('foo');
-        print 'foo';
-    }
+        public function testExpectFooActualFoo()
+        {
+            /*$this->expectOutputString('foo');
+            print 'foo';*/
+            echo 'nei';
+        }
 
-    public function testExpectBarActualBaz()
-    {
-        $this->expectOutputString('bar');
-        print 'baz';
+        public function testExpectBarActualBaz()
+        {
+            $this->expectOutputString('bar');
+            print 'baz';
+        }
     }
-}
 ?>
